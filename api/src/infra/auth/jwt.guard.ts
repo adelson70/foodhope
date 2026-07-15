bbeae59acacf0319fd6ceb754e707bb858e469dc
adelson70 <bittencourtadelson1@gmail.com>
2026-07-15 13:08:24 -0300

    incremento de aviso de n autorizado no jwt

--- api/src/infra/auth/jwt.guard.ts
+++ api/src/infra/auth/jwt.guard.ts
@@ -1,6 +1,18 @@
-import { Injectable } from '@nestjs/common';
-
+import {
+    Injectable,
+    UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { TokenExpiredError } from 'jsonwebtoken';
 
 @Injectable()
-export class JwtGuard extends AuthGuard('jwt') {}
+export class JwtGuard extends AuthGuard('jwt') {
+    handleRequest(err: any, user: any, info: any) {
+
+        if (info?.name === "TokenExpiredError" || info?.message === 'No auth token' || err || !user) {
+            throw new UnauthorizedException('Operação não autorizada');
+        }
+
+        return user;
+    }
+}
